e87651dc4cb1e589af3475177dc40bac8c04294d
arifullinrd <arifullinrd@lad24.ru>
2022-12-14 18:44:48 +0600

    sphere

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+import './App.css'
+
+export default function App() {
+  return (
+      <Spline scene="https://prod.spline.design/VLZnQ19L7N4rfGeI/scene.splinecode" />
+  );
+}
+
