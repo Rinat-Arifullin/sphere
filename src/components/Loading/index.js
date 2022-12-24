dd1025c747594f1f5c855ca7d81fdd8aeef6fa29
arifullinrd <arifullinrd@lad24.ru>
2022-12-25 01:31:27 +0600

    правки

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 
 import './loading.css';
 
-export default function Loading (isLoading){
+export default function Loading ({isLoading}){
 
     if(!isLoading){
         return null
