f77a74158d6a2c611119823b30fb74ec20c098f2
arifullinrd <arifullinrd@lad24.ru>
2022-12-25 21:24:54 +0600

    правки

--- src/App.js
+++ src/App.js
@@ -5,26 +5,39 @@ import NewsList from "./components/NewsList";
 
 export default function App() {
     const [newsLoading, setNewsLoading] = useState(true)
-    const [newsList, setNewsList] = useState([])
+    const [goodNews, setGoodNews] = useState([])
+    const [bedNews, setBedNews] = useState([])
 
     useEffect(()=>{
         fetch('http://127.0.0.1:8000/api/v1/get-list-view').then(data=>{
             return data.json()
         }).then(res=>{
-                console.log(res)
-                setNewsList(res)
+                const goodNews = []
+                const bedNews = [];
+                const setOfNews = new Set()
+                res.forEach(item=>{
+                    setOfNews.add(item.raiting)
+                    if(item.raiting){
+                        goodNews.push(item)
+                    }else{
+                        bedNews.push(item)
+                    }
+                })
+            setGoodNews(goodNews)
+            setBedNews(bedNews)
                 setNewsLoading(false)
             })
     },[])
 
   return (
       <>
-          <div className="badWrapper">
-            <NewsList newsList={newsList} side={'left'}/>
-          </div>
+          {goodNews.length && <div className="badWrapper">
+              <NewsList newsList={goodNews} side={'left'}/>
+          </div>}
+
         <Sphere/>
           <div className='goodWrapper'>
-            <NewsList newsList={newsList} side={'right'}/>
+            <NewsList newsList={goodNews} side={'right'}/>
           </div>
       </>
 
